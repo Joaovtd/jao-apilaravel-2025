7b4f2cf66485070f72a467e1eb1d1fbc024218ba
Joaovtd <104913839+Joaovtd@users.noreply.github.com>
2025-09-30 11:08:24 -0300

    aula - 30/09/25

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Tarefa;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +15,50 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+     //   User::factory()->create([
+     //       'name' => 'Test User',
+     //       'email' => 'test@example.com',
+     //   ]);
+     Tarefa::insert([
+            [
+                "texto" => "Preparar o café da manhã"
+            ],
+            [
+                "texto" => "Verificar a agenda e e-mails"
+            ],
+            [
+                "texto" => "Levar as crianças para a escola (se aplicável)"
+            ],
+            [
+                "texto" => "Fazer uma pausa de 15 minutos e alongar"
+            ],
+            [
+                "texto" => "Almoçar e hidratar-se"
+            ],
+            [
+                "texto" => "Responder a chamadas e mensagens importantes"
+            ],
+            [
+                "texto" => "Lavar a louça ou organizar a cozinha"
+            ],
+            [
+                "texto" => "Planejar as tarefas do dia seguinte"
+            ],
+            [
+                "texto" => "Caminhar com o cachorro (se tiver)"
+            ],
+            [
+                "texto" => "Ler um livro ou assistir algo relaxante"
+            ],
+            [
+                "texto" => "Jantar e passar tempo com a família"
+            ],
+            [
+                "texto" => "Arrumar a cama e o quarto"
+            ],
+            [
+                "texto" => "Escovar os dentes e fazer a higiene noturna"
+            ]
+    ]);
+}
 }
